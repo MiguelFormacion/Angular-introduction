0cae99a885aa611c01a7585c6f7c9eb2106b7ef8
MiguelFormacion <miguel.manzanares.pruebas@gmail.com>
2019-06-25 14:28:15 +0200

    Primer intento. Fran y Miguel

---
+++ iteracion1/angular-introduction1/src/app/app.component.ts
@@ -0,0 +1,22 @@
+import { Component } from '@angular/core';
+
+@Component({
+  selector: 'ai-root',
+  templateUrl: './app.component.html',
+  styleUrls: ['./app.component.scss']
+})
+export class AppComponent {
+  title = 'National Parks';
+
+ photo1 = "../assets/leon1.jpg";
+
+ photo2= "../assets/suricata.jpg";
+
+ photo3= "../assets/jabali.jpg";
+
+ description1 = "El rey de la selva";
+
+ description2 = "Fucking master";
+
+ description3 = "El más limpio y sanote de todos";
+}
